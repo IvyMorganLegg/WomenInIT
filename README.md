59d60eef7fc2c2cfc5024691256611f9b124d8d8
LitProductions <cdhslitproductions@gmail.com>
2019-03-21 19:16:29 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+womeninit-master
---
+++ js/scripts.js
@@ -0,0 +1,112 @@
+$(document).ready(function() {
+
+	/***************** Waypoints ******************/
+
+	$('.wp1').waypoint(function() {
+		$('.wp1').addClass('animated fadeInUp');
+	}, {
+		offset: '70%'
+	});
+	$('.wp2').waypoint(function() {
+		$('.wp2').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp3').waypoint(function() {
+		$('.wp3').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp4').waypoint(function() {
+		$('.wp4').addClass('animated fadeInUp');
+	}, {
+		offset: '97%'
+	});
+
+
+	/***************** Steps Animate ******************/
+
+	$(".faded").each(function(i) {
+	  $(this).delay(i * 400).fadeIn();
+	});
+
+
+	/***************** Initiate Flexslider ******************/
+	$('.flexslider').flexslider({
+		animation: "slide"
+	});
+
+	/***************** Initiate Fancybox ******************/
+
+	$('.single_image').fancybox({
+		padding: 4,
+	});
+
+	/***************** Nav Transformicon ******************/
+
+	/* When user clicks the Icon */
+	$('.nav-toggle').click(function() {
+		$(this).toggleClass('active');
+		$('.header-nav').toggleClass('open');
+		event.preventDefault();
+	});
+	/* When user clicks a link */
+	$('.header-nav li a').click(function() {
+		$('.nav-toggle').toggleClass('active');
+		$('.header-nav').toggleClass('open');
+
+	});
+
+	/***************** Header BG Scroll ******************/
+
+	$(function() {
+		$(window).scroll(function() {
+			var scroll = $(window).scrollTop();
+
+			if (scroll >= 20) {
+				$('section.navigation').addClass('fixed');
+				$('header').css({
+					"border-bottom": "none",
+					"padding": "35px 0"
+				});
+				$('header .member-actions').css({
+					"top": "36px",
+				});
+				$('header .navicon').css({
+					"top": "34px",
+				});
+			} else {
+				$('section.navigation').removeClass('fixed');
+				$('header').css({
+					"border-bottom": "solid 2px #fff",
+					"padding": "50px 0"
+				});
+				$('header .member-actions').css({
+					"top": "51px",
+				});
+				$('header .navicon').css({
+					"top": "48px",
+				});
+			}
+		});
+	});
+	/***************** Smooth Scrolling ******************/
+
+	$(function() {
+
+		$('a[href*=#]:not([href=#])').click(function() {
+			if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+					}, 1000);
+					return false;
+				}
+			}
+		});
+
+	});
+});
